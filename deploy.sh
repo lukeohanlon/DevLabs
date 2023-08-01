3af9577ccfe305b2d6f7adfb35c773e3ae056a73
luke0han <lukeohanlon@outlook.com>
2023-08-01 15:20:30 +0100

    deployment

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+
+sudo apt update && sudo apt install nodejs npm
+
+sudo npm install -g pm2
+
+pm2 stop example_app
+
+cd DevLabs
+
+npm install
+
+pm2 start ./bin/www --name example_app --env production
